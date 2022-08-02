9a6d3d5ea91e00d2919410093255311b1f075f29
arzen07 <57832174+arzen07@users.noreply.github.com>
2022-08-02 17:15:24 -0400

    This is my first commit with dear bhoge

---
+++ whatsapp-mern/src/Chat.js
@@ -0,0 +1,77 @@
+import React, {useState} from 'react';
+import {Avatar, IconButton} from "@mui/material";
+import "./Chat.css";
+import SearchOutlined from '@mui/icons-material/SearchOutlined';
+import AttachFile from '@mui/icons-material/AttachFile';
+import MoreVert from '@mui/icons-material/MoreVert';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import MicIcon from '@mui/icons-material/Mic';
+import axios from "./axios";
+
+function Chat( {messages} ) {
+ const [input, setInput] = useState("");
+
+const sendMessage = async (e) => {
+    e.preventDefault();
+    
+   await axios.post("/messages/new", {
+        message: input,
+        name: "DEMO NAME",
+        timestamp: "DEMO TIME",
+        received: false,
+    });
+
+    setInput("");
+};
+
+return (
+    <div className='chat'>
+        <div className='chat__header'>
+            <Avatar/>
+
+
+            <div className= 'chat__headerInfo'>
+                <h3>Room name</h3>
+                <p>Last seen at...</p>
+            </div>
+
+            <div className='chat__headerRight'>
+                <IconButton>
+                    <SearchOutlined/>
+                </IconButton>
+                <IconButton>
+                    <AttachFile/>
+                </IconButton>
+                <IconButton>
+                    <MoreVert/>
+                </IconButton>
+            </div>
+        </div>
+
+        <div className='chat__body'>
+            {messages.map((message) => (
+            <p className={`chat__message ${message.received && "chat__receiver"}`}>
+                <span className='chat__name'>Arjan</span>
+                {message.message}
+                <span className='chat__timestamp'>{message.timestamp}</span>
+                </p>
+            ))}
+
+        </div>
+
+        <div className='chat__footer'>
+            <InsertEmoticonIcon/>
+            <form>
+                <input value={input} onChange={e => setInput(e.target.value)} 
+                placeholder='Type a message'
+                type="text" />
+                <button onClick={sendMessage} type="submit">Send a message</
+                button>
+            </form>
+            <MicIcon/>
+        </div>
+    </div>
+  )
+}
+
+export default Chat;
---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC9CHEVuHRTM6II_VC9AxzvVGgwYxzvgHA",
+    authDomain: "whatsapp-mern-8af87.firebaseapp.com",
+    projectId: "whatsapp-mern-8af87",
+    storageBucket: "whatsapp-mern-8af87.appspot.com",
+    messagingSenderId: "504811030030",
+    appId: "1:504811030030:web:88ba5d6bec6be7e1d61e5f"
+  };
